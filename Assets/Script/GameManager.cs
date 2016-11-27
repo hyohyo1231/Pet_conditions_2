51cbe6ccb9ef11d4bda4ffad4594b2b6b374db0e
powerkome <powerkome@naver.com>
2016-11-27 13:46:40 +0900

    Start

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    // SINGLETON /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    static GameManager instance = null;
+
+    // DontDestroyOnLoad 제어를 위한 변수
+    private static int count = 0;
+    private int index;
+
+    public static GameManager Inst
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        instance = this;
+
+        index = count;
+        count++;
+
+        if (index == 0)
+            // 절대 삭제 불가 요청
+            DontDestroyOnLoad(this);
+        else
+            Destroy(gameObject);
+    }
+
+
+    // Game Data /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 여기에 게임 필요 변수 선언.
+    public bool isGameStop = false;
+
+
+
+
+    
+
+
+
+    // 모든 관계 연결.
+    public PlayerPet playerPet;
+    public Transform originalPos;                       // 펫의 처음 위치.
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // Play Init.
+    public bool isPlayReady = false;
+    public void InitStatePlay()
+    {
+        // 1. 펫 대기상태.
+        // isPlayReady = false;
+    }
+
+}
---
+++ Assets/Script/PlayerPet.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Pet State Define.
+public enum PetState
+{
+    Idle, Eat, Play, Walk, Max
+};
+
+public class PlayerPet : MonoBehaviour {
+
+    // Delegate Define //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    delegate void State();
+    State[] petState = null;
+
+    // Pet Data /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 여기에 Pet 필요 변수 선언.
+    PetState state;
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    void Start()
+    {
+        petState = new State[(int)PetState.Max] { Idle, Eat, Play, Walk };
+
+        // Init.
+        Init();
+    }
+
+	public void Init()
+    {
+        // Init Define.
+        state = PetState.Play;
+
+        switch (state)
+        {
+            case PetState.Idle:
+                break;
+            case PetState.Eat:
+                break;
+            case PetState.Play:
+                GameManager.Inst.InitStatePlay();
+
+                break;
+            case PetState.Walk:
+                break;
+        }
+       
+    }
+
+
+    // Pet State별 function. /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void Idle()
+    {
+
+    }
+    void Eat()
+    {
+        
+    }
+    void Play()
+    {
+        
+    }
+
+    void Walk()
+    {
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    void Update ()
+    {
+        if (GameManager.Inst.isGameStop) return;
+        petState[(int)state]();
+    }
+}
